703f9bbaacfa0d0feb66b6fcfdf1e6a0b9d4fa7e
garamkim83 <garamk83@gmail.com>
2022-07-14 00:11:27 +0900

    Update main.rs

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -8,23 +8,25 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("The secret number is {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new(); //mut:  가변의
 
-    let mut guess = String::new(); //mut:  가변의
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    // 타입 변경
-    let guess:u32=guess.trim().parse().expect("Please type a number!");
+        // 타입 변경
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
-    
-    match guess.cmp(&secret_number){                // 숫자 비교
-        Ordering::Less=>println!("Too small!"),     // 세미콜론 아님
-        Ordering::Greater=>println!("Too big!"),
-        Ordering::Equal=>println!("You win!"),
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            // 숫자 비교
+            Ordering::Less => println!("Too small!"), // 세미콜론 아님
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
