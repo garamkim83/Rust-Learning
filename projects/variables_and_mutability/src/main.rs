5277cfd7b4dbc19088efa5389c60ebcaa8f5bd8f
garamkim83 <garamk83@gmail.com>
2022-07-27 15:28:52 +0900

    Update main.rs

--- projects/variables_and_mutability/src/main.rs
+++ projects/variables_and_mutability/src/main.rs
@@ -1,21 +1,21 @@
 use std::io;
 
 fn main() {
-    let mut x =5;
+    let mut x = 5;
     println!("The value of x is {}", x);
-    x=6;
+    x = 6;
     println!("The value of x is {}", x);
 
     let y = 4;
     println!("The value of y is {}", y);
-    
+
     //constant
-    const THREE_HOURS_IN_SECONDS: u32 = 60*60*3;
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("3 hours = {} seconds", THREE_HOURS_IN_SECONDS);
 
     //shadowing
     {
-        let y = y*2;
+        let y = y * 2;
         println!("The value of y in the inner scope is: {}", y);
     }
 
@@ -24,5 +24,5 @@ fn main() {
     let spaces = "    ";
     println!("{}", spaces);
     let spaces = spaces.len();
-    println!("{}",spaces);
+    println!("{}", spaces);
 }
