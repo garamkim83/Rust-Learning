b818b0b1c142de6a0614d8eccd1c4f35a10e1de3
garamkim83 <garamk83@gmail.com>
2022-07-31 23:01:36 +0900

    Update main.rs

--- projects/variables_and_mutability/src/main.rs
+++ projects/variables_and_mutability/src/main.rs
@@ -61,4 +61,27 @@ fn main() {
     println!("The array a[1] is {}", a[1]);
     let a = [3; 5];
     println!("The array a[1] is {}", a[1]);
+
+    //array example
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number.");
+
+    let element = a[index];
+
+    println!(
+        "The value od the element at index {} is: {}",
+        index, element
+    );
 }
