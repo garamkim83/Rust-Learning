bd84406f944b56179dd9878c5bfe0485c92e6cee
garamkim83 <garamk83@gmail.com>
2022-08-06 00:03:12 +0900

    Update main.rs

--- projects/loop_practice/src/main.rs
+++ projects/loop_practice/src/main.rs
@@ -28,11 +28,25 @@ fn main() {
     };
     println!("The result is {}", result);
 
-    //conditinal loops with while
+    //conditinal loops with while 1
     let mut number = 3;
     while number != 0 {
         println!("{}!", number);
         number -= 1;
     }
     println!("LIFTOFF!!!");
+
+    //conditinal loops with while 1
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+        index += 1;
+    }
+
+    //looping through a collection with for
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("The value is: {}", element);
+    }
 }
