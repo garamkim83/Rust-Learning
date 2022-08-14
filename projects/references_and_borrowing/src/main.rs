f53ba92a63e5463ce9df45c11b97c4111462673d
garamkim83 <garamk83@gmail.com>
2022-08-14 21:00:34 +0900

    Update main.rs

--- projects/references_and_borrowing/src/main.rs
+++ projects/references_and_borrowing/src/main.rs
@@ -2,10 +2,19 @@ fn main() {
     let s1 = String::from("hello");
     let len = calculate_length(&s1); //the owner doesn't change
                                      //reference로 받아옴
+                                     //수정 불가능
 
     println!("The length of '{}' is {}", s1, len);
+
+    //Mutable references
+    let mut s = String::from("hello");
+    change(&mut s);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
