fa05e09e1654e97fd99e47d71dcca77739cb7b7e
garamkim83 <garamk83@gmail.com>
2022-08-15 16:07:42 +0900

    Update main.rs

--- projects/references_and_borrowing/src/main.rs
+++ projects/references_and_borrowing/src/main.rs
@@ -9,6 +9,16 @@ fn main() {
     //Mutable references
     let mut s = String::from("hello");
     change(&mut s);
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{} and {}", r1, r2);
+    //variables r1 and r2 will not be used after this point
+    let r3 = &mut s;
+    println!("{}", r3);
+
+    //Dangling References
+    let references_to_nothing=dangle();
 }
 
 fn calculate_length(s: &String) -> usize {
@@ -17,4 +27,10 @@ fn calculate_length(s: &String) -> usize {
 
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
+}
+
+fn dangle()->String{    // not '&s'
+    let s=String::from("hello");
+
+    s   // not '&s'
 }
