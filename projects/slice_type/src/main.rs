1e8c27d680fb8cd438852729a493b3a9f11d6bf3
garamkim83 <garamk83@gmail.com>
2022-08-17 20:54:36 +0900

    Update main.rs

--- projects/slice_type/src/main.rs
+++ projects/slice_type/src/main.rs
@@ -13,9 +13,25 @@ fn main() {
     let slice2 = &s[3..len];
     let slice3 = &s[3..];
     let slice4 = &s[0..len]; //[..]과 동일
+
+    //String Slices as Parameter
+    let my_string = String::from("hello world");
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+    let word = first_word(my_string_literal);
+
+    //Other Slices
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
