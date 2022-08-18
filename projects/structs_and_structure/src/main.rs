766e94087de2c9bbb30369d73846635e7d9dd317
garamkim83 <garamk83@gmail.com>
2022-08-19 00:14:40 +0900

    Update main.rs

--- projects/structs_and_structure/src/main.rs
+++ projects/structs_and_structure/src/main.rs
@@ -6,6 +6,24 @@ fn main() {
         sign_in_count: 1,
     };
     user1.email = String::from("anotheremail.example.com");
+
+    //Creating instances from another instances with struct update syntax
+    let user2 = User {
+        email: String::from("anoter@example.com"),
+        username: user1.username,
+        active: user1.active,
+        sign_in_count: 1,
+    };
+
+    let user3 = User {
+        email: String::from("another3@example.com"),
+        ..user2 //rest of the values from user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
 }
 
 struct User {
@@ -14,6 +32,13 @@ struct User {
     email: String,
     sign_in_count: u64,
 }
+
+//tuple structs
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+//unit-like structs
+struct AlwaysEqual;
 
 fn build_user(email: String, username: String) -> User {
     User {
