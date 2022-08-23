a39723ace16c8267cc00646d5a4328848bc4a945
garamkim83 <garamk83@gmail.com>
2022-08-23 23:50:42 +0900

    Update main.rs

--- projects/method_syntax_defining/src/main.rs
+++ projects/method_syntax_defining/src/main.rs
@@ -3,10 +3,17 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
-
+/*
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+}
+*/
+
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
     }
 }
 
@@ -16,8 +23,14 @@ fn main() {
         height: 50,
     };
 
+    /*
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+    */
+
+    if rect1.width() {
+        println!("The rectangle has a non-zero width; it is {}", rect1.width);
+    }
 }
