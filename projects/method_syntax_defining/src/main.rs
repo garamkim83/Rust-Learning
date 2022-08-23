b2f73e6f604f92bc514f94cbec516de474a1f134
garamkim83 <garamk83@gmail.com>
2022-08-24 00:02:15 +0900

    Update main.rs

--- projects/method_syntax_defining/src/main.rs
+++ projects/method_syntax_defining/src/main.rs
@@ -3,19 +3,24 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
-/*
+
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
-}
-*/
 
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+/*
 impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
 }
+*/
 
 fn main() {
     let rect1 = Rectangle {
@@ -30,7 +35,27 @@ fn main() {
     );
     */
 
+    /*
     if rect1.width() {
         println!("The rectangle has a non-zero width; it is {}", rect1.width);
     }
+    */
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
